4d20eeaebb40ac6bb3d9822f85bcf43e6fad7e55
txabiii <txabiguerrero2000@gmail.com>
2023-05-03 23:19:18 +0800

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
-import styles from './page.module.css'
+import styles from './page.module.scss'
 
 export default function Home() {
   return(
-    <div>
-      <h1>Welcome home</h1>
+    <div className={styles.home}>
+      <main>
+        {/* HERO SECTION */}
+        <section className={styles.feature}>
+          <div className={styles.top}>
+            <div className={styles.header}>
+              <div className={styles.brand}>
+                <h4>GENIEOUS</h4>
+                <h6>CREATIVE WRITING</h6>
+              </div>
+              <div className={styles.beta}>BETA</div>
+            </div>
+            <div className={styles.content}>
+              <h1>World-building for the future</h1>
+              <h3><i>Human-first, AI-assisted</i></h3>
+              <p>Hear ye! Hear ye! A new era of creative writing has come upon us. With Genieous Creative Writing, with the help of AI has never been easier than before. The only limitations are your mind and will-power.</p>
+            </div>
+          </div>
+          <div className={styles.bottom}>
+
+          </div>
+        </section>
+        {/* FEATURE #1 */}
+        <section className={styles.feature}>
+          <div className={styles.top}>
+
+          </div>
+          <div className={styles.bottom}>
+
+          </div>
+        </section>
+        <section className={styles.feature}>
+          {/* ADD FEATURE */}
+        </section>
+        <section className={styles.feature}>
+          {/* ADD FEATURE */}
+        </section>
+      </main>
+      <aside>
+
+      </aside>
     </div>
   )
 }
