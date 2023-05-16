d62efb056a660d3689932de47e59b3c86d104fb5
txabiii <txabiguerrero2000@gmail.com>
2023-05-16 23:52:39 +0800

    More Editor Tools

--- src/app/workspace/page.tsx
+++ src/app/workspace/page.tsx
@@ -5,17 +5,43 @@ import 'draft-js/dist/Draft.css';
 
 import Footer from '@root/components/Footer/component';
 import Button from '@root/components/Button/component';
+import EditToolbar from '@root/components/EditToolbar/component';
 
 import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react";
 
-import { Editor, EditorState } from 'draft-js';
+import { ContentBlock, Editor, EditorState } from 'draft-js';
 
 import { logOut  as userClientLogOut, checkVerification } from "@root/api/userClient";
 
+function myBlockStyleFn(contentBlock: ContentBlock): string {
+  const type = contentBlock.getType();
+  if (type === 'blockquote') {
+    return styles.customBlockQuote;
+  }
+  return '';
+}
+
 const isSSR = typeof window === 'undefined'; 
 
+interface Property {
+  id: number;
+  name: string;
+  value: string;
+}
+
+interface Object {
+  id: number;
+  name: string;
+  type: string;
+  description: string;
+  properties: Property[];
+}
+
 export default function Workspace() {
+  // object state
+  const [object, setObject] = useState<Object>()
+
   const router = useRouter()
 
   const logOut = () => {
@@ -55,16 +81,19 @@ export default function Workspace() {
               <Button content='Log out' click={logOut} />
             </div>
             <div className={styles.tools}>
-
+              <EditToolbar editorState={editorState} setEditorState={setEditorState} />
             </div>
           </nav>
           <section className={styles.workArea}>
             <div className={styles.document}>
               <h5>Object details</h5>
+              <input type="text" className={styles.name} placeholder='Write name of object' />
+              <input type="text" className={styles.type} placeholder='Write type of object'/>
               <Editor 
                 editorState={editorState} 
-                onChange={setEditorState} 
+                onChange={setEditorState}
                 placeholder='Write the description of your object'
+                blockStyleFn={myBlockStyleFn}
               />
             </div>
             <aside className={styles.properties}>
---
+++ src/components/EditToolbar/component.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { EditorState, RichUtils } from "draft-js";
+
+import styles from './component.module.scss'
+import cx from 'classnames'
+
+type StyleButtonProps = {
+  onClick?: () => void;
+  onToggle?: (style: string) => void;
+  active?: boolean;
+  className?: string;
+  style?: string;
+  label: string;
+  iconName?: string;
+};
+
+const StyleButton = (props: StyleButtonProps) => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (props?.onToggle && props?.style) {
+      props.onToggle(props.style);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={cx(styles.toolButton, { [styles.active] : props.active})}
+    >
+      {props.label}
+    </button>
+  );
+};
+
+var INLINE_STYLES = [
+  { label: "Bold", style: "BOLD" },
+  { label: "Italic", style: "ITALIC" },
+  { label: "Underline", style: "UNDERLINE" }
+];
+
+const InlineStyleControls = (props: any) => {
+  const currentStyle = props.editorState.getCurrentInlineStyle();
+
+  return (
+    <div className={styles.toolGroup}>
+      {INLINE_STYLES.map((type) => (
+        <StyleButton
+          key={type.label}
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      ))}
+    </div>
+  );
+};
+
+const BLOCK_TYPES = [
+  { label: "H1", style: "header-one" },
+  { label: "H2", style: "header-two" },
+  { label: "H3", style: "header-three" },
+  { label: "H4", style: "header-four" },
+  { label: "Section", style: "header-five" },
+  { label: "Blockquote", style: "blockquote" }
+];
+const BlockStyleControls = (props: any) => {
+  const { editorState } = props;
+  const selection = editorState.getSelection();
+  const blockType = editorState
+    .getCurrentContent()
+    .getBlockForKey(selection.getStartKey())
+    .getType();
+
+  return (
+    <div className={styles.toolGroup}>
+      {BLOCK_TYPES.map((type) => (
+        <StyleButton
+          key={type.label}
+          active={type.style === blockType}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      ))}
+    </div>
+  );
+};
+
+type EditToolbarProps = {
+  editorState: EditorState;
+  setEditorState: React.Dispatch<React.SetStateAction<EditorState>>;
+}
+
+export default function EditToolbar({ editorState, setEditorState}: EditToolbarProps) {
+  const toggleInlineStyle = (inlineStyle: string) => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, inlineStyle));
+  };
+
+  const toggleBlockType = (blockType: string) => {
+    setEditorState(RichUtils.toggleBlockType(editorState, blockType));
+  };
+
+  return (
+    <div>
+      <InlineStyleControls editorState={editorState} onToggle={toggleInlineStyle} />
+      <BlockStyleControls editorState={editorState} onToggle={toggleBlockType} />
+    </div>
+  )
+}
