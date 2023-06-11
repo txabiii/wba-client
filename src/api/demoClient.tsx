45a191476bbce629ca26ca0dc05822a95e3a0090
txabiii <txabiguerrero2000@gmail.com>
2023-06-11 17:23:54 +0800

    Update demoClient.tsx

--- src/api/demoClient.tsx
+++ src/api/demoClient.tsx
@@ -1,5 +1,5 @@
 // const baseUrl = 'http://127.0.0.1:5000'
-const baseUrl = 'https://wba-server.vercel.app/'
+const baseUrl = 'https://wba-server.vercel.app'
 
 interface DemoResponse {
   status: string;
